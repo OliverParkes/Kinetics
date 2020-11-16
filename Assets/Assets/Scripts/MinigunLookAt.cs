ce0f7a2a7d29250a172057b58d2066e803d3f6cb
Oliver parkes <oliverparkes@hotmail.com>
2020-11-16 20:01:42 +1030

    Changed Minigun again

--- Assets/Assets/Scripts/MinigunLookAt.cs
+++ Assets/Assets/Scripts/MinigunLookAt.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class MinigunLookAt : MonoBehaviour
 {
     public Transform Player;
-    public float Speed;
+    private float Speed;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        Speed = GameManager.Wave;
         Vector3 Position = Player.transform.position - transform.position;
         Quaternion Rotate = Quaternion.LookRotation(Position);
         transform.rotation = Quaternion.Lerp(transform.rotation, Rotate, Speed*Time.deltaTime);
