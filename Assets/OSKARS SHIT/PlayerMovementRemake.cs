f2d54e8e02ae8fc5fdc86cf3663857ca2ec81dd1
IJustDrankALavaLamp <IJustDrankALavaLamp@users.noreply.github.com>
2020-11-16 20:00:57 +1030

    working on the player controller

---
+++ Assets/OSKARS SHIT/PlayerMovementRemake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementRemake : MonoBehaviour
+{
+    public Rigidbody Aoba;
+
+    private float sensitivity = 50f;
+    private float sensMultiplier = 1f;
+
+    public float rotationSpeed = 1f;
+
+    void Start()
+    {
+        Aoba = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    void Update()
+    {
+        Aoba.rotation = Quaternion.Euler(Aoba.rotation.eulerAngles + new Vector3(rotationSpeed * Input.GetAxis("Mouse Y") * -1, rotationSpeed * Input.GetAxis("Mouse X"), 0f));
+    }
+}
