e3496adfd993e85bb92639a0325f8ae69bdb1351
Oliver parkes <oliverparkes@hotmail.com>
2020-11-16 19:41:33 +1030

    Changed minigun and Pchan settings

--- Assets/Assets/Scripts/MinigunLookAt.cs
+++ Assets/Assets/Scripts/MinigunLookAt.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class MinigunLookAt : MonoBehaviour
 {
-
     public Transform Player;
+    public float Speed;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.LookAt(Player);
+        Vector3 Position = Player.transform.position - transform.position;
+        Quaternion Rotate = Quaternion.LookRotation(Position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Rotate, Speed*Time.deltaTime);
     }
     
 }
